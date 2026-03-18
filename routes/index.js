4b84b90d12b4079092b6456cdf95c054d8c1e902
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-03-18 14:08:07 +0530

    practice

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
+const passport = require('passport');
 var router = express.Router();
+const userModel = require("./users")
+
+
+
+const localStrategy = require("passport-local");
+passport.use(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
+router.get('/profile', function(req, res){
+  res.send('Welcome to profile');
+})
+
+
+
+router.post("/register", function(req, res){
+  var userdata = new userModel({
+    username: String,
+    secret: String
+  });
+
+  userModel.register(userdata,req.body.password)
+  .then(function(registerduser){
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/profile");
+    })
+  })
+});
 module.exports = router;
